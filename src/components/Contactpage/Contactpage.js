d7ab663e4caf2ccfe72cc61ea47e1b75880c5022
Hemant Arora <65280839+hemantarora81@users.noreply.github.com>
2023-05-31 12:04:50 +0530

    Update Contactpage.js

--- src/components/Contactpage/Contactpage.js
+++ src/components/Contactpage/Contactpage.js
@@ -8,7 +8,8 @@ function Contactpage() {
       <Container>
         <h2 className="contacthead">Get In Touch</h2>
         <p className="contactpara">
-          I’m currently searching for opportunities for a front-end developer
+          Currently I'm doing Internship as ReactJs Developer At Eazy Erp Technologies Pvt Ltd.
+          I’m searching for opportunities for a front-end developer
           role. <br /> If there is any vacancy my inbox is always open. Whether
           <br /> you have any further questions or just want to say hi, <br />
           I’ll try my best to get back to you!
